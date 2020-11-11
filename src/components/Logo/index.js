bf9e6be6eb33196d78a9b4477eb4ac56683050a6
Rubykks <valentin64.genest@gmail.com>
2020-11-11 16:41:52 +0100

    Centered logo Auth

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import LogoAdapt from './img/Adapt_logo.svg'
-import styles from './Logo.module.css'
-const Logo = () => {
+import React from "react";
+import LogoAdapt from "./img/Adapt_logo.svg";
+import styles from "./Logo.module.css";
 
-    return (
-      <div className={styles.auth_container}>
-        <div className={styles.logo_container}>
-      <img className={styles.Logo} src={LogoAdapt} alt="Logo Adapt"/>
-      </div>
-      </div>
-    );
-  }
-  
-  export default Logo;
-  
+const Logo = () => {
+  return (
+    <div>
+      <img className={styles.Logo} src={LogoAdapt} alt="Logo Adapt" />
+    </div>
+  );
+};
+
+export default Logo;
--- src/pages/Authentification/index.js
+++ src/pages/Authentification/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import Logo from '../../components/Logo';
+import React from "react";
+import Logo from "../../components/Logo";
+import styles from "./Authentification.module.css";
+
 const Authentification = () => {
-    return (
-     
-      <Logo/>
-    
-    );
-  }
-  
-  export default Authentification;
+  return (
+    // <div className={styles.authContainer}>
+    <div className={styles.logoContainer}>
+      <Logo />
+    </div>
+    // </div>
+  );
+};
+
+export default Authentification;
