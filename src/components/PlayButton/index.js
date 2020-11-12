520db2047e3e8fdcf13a99fe48d6e46bf750f1bd
Rubykks <valentin64.genest@gmail.com>
2020-11-12 14:22:31 +0100

    Play button functional (anim)

--- src/components/PlayButton/index.js
+++ src/components/PlayButton/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Lottie from "react-lottie";
 import styles from "./PlayButton.module.css";
 import animationDataBlob from "./assets/blob.json";
 import pauseIcon from "./assets/pause.svg";
-// import playIcon from "./assets/play.svg";
+import playIcon from "./assets/play.svg";
 
 const PlayButton = () => {
   const defaultOptionsBlob = {
@@ -13,6 +13,16 @@ const PlayButton = () => {
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
+  };
+
+  const [src, updateSrc] = useState(playIcon);
+
+  const togglePlay = () => {
+    if (src === pauseIcon) {
+      updateSrc(playIcon);
+    } else {
+      updateSrc(pauseIcon);
+    }
   };
 
   return (
@@ -25,12 +35,8 @@ const PlayButton = () => {
           width={300}
         />
         <div className={styles.playPauseButtonContainer}>
-          <button className={styles.playPauseButton}>
-            <img
-              className={styles.pauseIcon}
-              src={pauseIcon}
-              alt="pause icon"
-            />
+          <button className={styles.playPauseButton} onClick={togglePlay}>
+            <img className={styles.pauseIcon} src={src} alt="pause icon" />
           </button>
         </div>
       </div>
